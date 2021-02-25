e6f5179be878ee65998316ea2a2b96ec5e145f5b
firefart <firefart@gmail.com>
2021-02-25 23:53:38 +0100

    prod checks

--- prod.sh
+++ prod.sh
@@ -5,10 +5,10 @@ set -euf -o pipefail
 # check for needed config files
 # these are mounted using docker-compose and are
 # required by the setup
-[ ! -f RT_SiteConfig.pm ] && echo "RT_SiteConfig.pm does not exist."
-[ ! -f ./msmtp/msmtp.conf ] && echo "./msmtp/msmtp.conf does not exist."
-[ ! -f ./nginx/certs/pub.pem ] && echo "./nginx/certs/pub.pem does not exist."
-[ ! -f ./nginx/certs/priv.pem ] && echo "./nginx/certs/priv.pem does not exist."
+[ ! -f RT_SiteConfig.pm ] && { echo "RT_SiteConfig.pm does not exist."; exit 1; }
+[ ! -f ./msmtp/msmtp.conf ] && { echo "./msmtp/msmtp.conf does not exist."; exit 1; }
+[ ! -f ./nginx/certs/pub.pem ] { && echo "./nginx/certs/pub.pem does not exist."; exit 1; }
+[ ! -f ./nginx/certs/priv.pem ] { && echo "./nginx/certs/priv.pem does not exist."; exit 1; }
 
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1

42c68a5c33f9f0d5d05c6e7c8cc502038635bc30
Christian Mehlmauer <105281+firefart@users.noreply.github.com>
2025-09-16 18:04:19 +0200

    more work

--- dev-helm.sh
+++ dev-helm.sh
@@ -2,6 +2,16 @@
 
 set -euf -o pipefail
 
+echo "uninstalling old stuff"
+kubectl delete job --all --ignore-not-found
 helm uninstall --ignore-not-found rt
-sleep 5
+echo "sleeping 15 seconds to let things settle"
+sleep 15
+echo "installing new stuff"
 helm install rt helm/
+echo "sleeping 2 minutes to let the database come up"
+sleep 120
+echo "initializing the database"
+kubectl apply -f k8s-jobs/db-init.yaml
+echo "done"
+kubectl get pods

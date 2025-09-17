42a001b3ece27b9d6601d9c27b3df73a55605559
firefart <105281+firefart@users.noreply.github.com>
2025-09-17 23:22:59 +0200

    use secrets

--- dev-helm.sh
+++ dev-helm.sh
@@ -5,9 +5,14 @@ set -euf -o pipefail
 echo "uninstalling old stuff"
 kubectl delete job --all --ignore-not-found
 helm uninstall --ignore-not-found rt
+kubectl delete secret --all --ignore-not-found
 echo "sleeping 15 seconds to let things settle"
 sleep 15
 echo "installing new stuff"
+kubectl create secret generic rt-db-creds \
+    --from-literal=dbname=rt \
+    --from-literal=username=rt \
+    --from-literal=password='rt'
 helm install rt helm/
 echo "sleeping 2 minutes to let the database come up"
 sleep 120

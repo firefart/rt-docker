51814d1aa8280bbb80db0c3e7ba6ca311422c668
Christian Mehlmauer <105281+firefart@users.noreply.github.com>
2026-03-17 20:51:47 +0100

    update

--- dev.sh
+++ dev.sh
@@ -14,6 +14,7 @@ check_dev_files
 
 fix_file_perms
 
+docker compose -f docker-compose.yml -f docker-compose.dev.yml pull
 docker compose -f docker-compose.yml -f docker-compose.dev.yml stop
 docker compose -f docker-compose.yml -f docker-compose.dev.yml rm -f
 docker compose -f docker-compose.yml -f docker-compose.dev.yml --progress=plain build --pull
